392403bbe227039e9f151b51e6b98d93dd1d4134
Geert-Johan Riemer <geertjohan@geertjohan.net>
2020-09-30 13:28:41 +0200

    Bump dependencies again

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/matthewwhitaker/Projects/FlutterDevelopment/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/matthewwhitaker/Projects/Flutter_Projects/flutter_html/example"
+export "FLUTTER_ROOT=/home/geertjohan/apps/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/geertjohan/src/github.com/GeertJohan/flutter_html/example"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/matthewwhitaker/Projects/FlutterDevelopment/flutter/bin/cache/artifacts/engine/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/home/geertjohan/apps/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
